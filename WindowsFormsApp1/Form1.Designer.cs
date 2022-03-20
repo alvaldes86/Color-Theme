3061ca69471cdc28294c37f3944b69967cdc743c
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2022-03-20 11:23:22 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorGroupBox = new System.Windows.Forms.GroupBox();
+            this.yellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.redRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.normalRadioButton = new System.Windows.Forms.RadioButton();
+            this.colorGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorGroupBox
+            // 
+            this.colorGroupBox.Controls.Add(this.normalRadioButton);
+            this.colorGroupBox.Controls.Add(this.redRadioButton);
+            this.colorGroupBox.Controls.Add(this.yellowRadioButton);
+            this.colorGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorGroupBox.Location = new System.Drawing.Point(166, 47);
+            this.colorGroupBox.Name = "colorGroupBox";
+            this.colorGroupBox.Size = new System.Drawing.Size(294, 436);
+            this.colorGroupBox.TabIndex = 0;
+            this.colorGroupBox.TabStop = false;
+            this.colorGroupBox.Text = "Select a Background Color";
+            // 
+            // yellowRadioButton
+            // 
+            this.yellowRadioButton.AutoSize = true;
+            this.yellowRadioButton.Location = new System.Drawing.Point(86, 67);
+            this.yellowRadioButton.Name = "yellowRadioButton";
+            this.yellowRadioButton.Size = new System.Drawing.Size(72, 22);
+            this.yellowRadioButton.TabIndex = 0;
+            this.yellowRadioButton.TabStop = true;
+            this.yellowRadioButton.Text = "Yellow";
+            this.yellowRadioButton.UseVisualStyleBackColor = true;
+            this.yellowRadioButton.CheckedChanged += new System.EventHandler(this.yellowRadioButton_CheckedChanged);
+            // 
+            // redRadioButton
+            // 
+            this.redRadioButton.AutoSize = true;
+            this.redRadioButton.Location = new System.Drawing.Point(86, 131);
+            this.redRadioButton.Name = "redRadioButton";
+            this.redRadioButton.Size = new System.Drawing.Size(56, 22);
+            this.redRadioButton.TabIndex = 2;
+            this.redRadioButton.TabStop = true;
+            this.redRadioButton.Text = "Red";
+            this.redRadioButton.UseVisualStyleBackColor = true;
+            this.redRadioButton.CheckedChanged += new System.EventHandler(this.redRadioButton_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 538);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // normalRadioButton
+            // 
+            this.normalRadioButton.AutoSize = true;
+            this.normalRadioButton.Location = new System.Drawing.Point(86, 199);
+            this.normalRadioButton.Name = "normalRadioButton";
+            this.normalRadioButton.Size = new System.Drawing.Size(133, 22);
+            this.normalRadioButton.TabIndex = 3;
+            this.normalRadioButton.TabStop = true;
+            this.normalRadioButton.Text = "Back to Normal";
+            this.normalRadioButton.UseVisualStyleBackColor = true;
+            this.normalRadioButton.CheckedChanged += new System.EventHandler(this.normalRadioButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 663);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.colorGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.colorGroupBox.ResumeLayout(false);
+            this.colorGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox colorGroupBox;
+        private System.Windows.Forms.RadioButton yellowRadioButton;
+        private System.Windows.Forms.RadioButton redRadioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton normalRadioButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void yellowRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (yellowRadioButton.Checked)
+            {
+                this.BackColor = Color.Yellow;
+            }
+        }
+
+        private void redRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (redRadioButton.Checked)
+            {
+                this.BackColor = Color.Red;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void normalRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (normalRadioButton.Checked)
+            {
+                this.BackColor = SystemColors.Control;
+            }
+        }
+    }
+}
